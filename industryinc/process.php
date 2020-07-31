805d4d9c1f57d5d998f188ad13841cd84c957ef7
Rishab Koul <rishabkoul2001@gmail.com>
2020-07-31 12:41:03 +0530

    initial commit

---
+++ industryinc/process.php
@@ -0,0 +1,22 @@
+<?php
+
+if (isset($_POST['btn-send'])) {
+    $UserName = $_POST['UName'];
+    $Email = $_POST['Email'];
+    $No = $_POST['no'];
+    $Address = $_POST['address'];
+    $Subject = $_POST['Subject'];
+    $Msg = $_POST['msg'];
+
+    if (empty($UserName) || empty($Email) || empty($Subject) || empty($Msg) || empty($No) || empty($Address)) {
+        header('location:contact.php?error');
+    } else {
+        $to = "info@saaswad.com";
+        $Msg = $Msg . " " . strval($No) . " " . $Address;
+        if (mail($to, $Subject, $Msg, $Email)) {
+            header('location:contact.php?success');
+        }
+    }
+} else {
+    header("location:contact.php");
+}
